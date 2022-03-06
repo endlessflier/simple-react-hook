e4c1abfd11f8f7def6ad3fc1005ac3b2da98b236
Full Stack Developer <webdeveloper2009@outlook.com>
2022-03-06 09:28:38 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <AppDemo2 />
+    </div>
+  );
+}
+
+function AppDemo2() {
+  const [darkMode, setDarkMode] = useState(false);
+  const enabledRef = useRef(true);
+
+  return (
+    <div className={`App ${darkMode && "dark-mode"}`}>
+      <h1>The useState hook</h1>
+      <h2>Click the button to toggle the state</h2>
+      <button
+        onClick={() => {
+          if (enabledRef.current) {
+            setDarkMode(!darkMode);
+            console.log(darkMode);
+            enabledRef.current = false;
+            setTimeout(() => (enabledRef.current = true), 2000);
+          }
+        }}
+      >
+        toggle dark mode
+      </button>
+    </div>
+  );
+}
